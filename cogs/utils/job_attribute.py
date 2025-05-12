dddc3d7dc3ce0e3a9a9da12523dbb189f199f9c0
kuanchiun <tungsega@yahoo.com.tw>
2025-05-13 01:15:35 +0800

    Add doc

--- cogs/utils/job_attribute.py
+++ cogs/utils/job_attribute.py
@@ -1,3 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional, Dict
 
+class JobBranch(ABC):
+    name: str
+    main_attribute: List[str]
+    dot_attribute: str
+    
+    
