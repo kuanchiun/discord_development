e0ddc8e366a343c81c9afda7ebb0f15fc2ae2194
kuanchiun <tungsega@yahoo.com.tw>
2025-05-21 01:04:29 +0800

    Update

---
+++ cogs/utils/equipinventory.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass, field
+from typing import Optional, Dict, List, Union
+
+from .equipment import Equipment
+
+@dataclass
+class EquipInventory:
+    ...
---
+++ cogs/utils/equipmentslot.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import Optional, Dict, List, Tuple
+
+from .equipment import Equipment
+
+@dataclass
+class EquipmentSlot:
+    weapon: Optional["Equipment"] = None
+    head: Optional["Equipment"] = None
+    chest: Optional["Equipment"] = None
+    leggings: Optional["Equipment"] = None
+    feet: Optional["Equipment"] = None
+    
+    earring: Optional["Equipment"] = None
+    necklace: Optional["Equipment"] = None
+    bracelet: Optional["Equipment"] = None
+    ring1: Optional["Equipment"] = None
+    ring2: Optional["Equipment"] = None
+    
+    def is_already_equipped(self, slot):
+        if not hasattr(self, slot):
+            raise ValueError(f"❌ 無效的裝備欄位: {slot}")
+        
+        return getattr(self, slot) is not None
+        
+    def equip(self, ):
+        ...
+    
+    
